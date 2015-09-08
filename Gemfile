bcc3d3e2ae7e1109c6849dfb8535f2e73a20cc4a
ayaka010816 <ayaka.hostos@gmail.com>
2015-09-08 13:50:57 +0000

    fontsize

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
+# Twitter Bootstrap
+
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -36,6 +36,13 @@ class MessagesController < ApplicationController
     end
   end
 private
+
+def message_params
+    # params[:message]のパラメータで name , bodyのみを許可する。
+    # 返り値は ex:) {name: "入力されたname" , body: "入力されたbody" }
+    params.require(:message).permit(:name, :body, :age)
+end
+  
   def set_message
     @message = Message.find(params[:id])
   end
